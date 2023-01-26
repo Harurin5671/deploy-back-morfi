3399ba239a332f1e44cb918a0773dc6b0be1a07e
Haru <frankcito639@gmail.com>
2023-01-26 08:50:04 -0500

    Deploy

--- api/src/controllers/categories.controller.js
+++ api/src/controllers/categories.controller.js
@@ -3,9 +3,8 @@ const { Categories } = require("../db");
 
 const getCategories = async (req, res) => {
   try {
-    const JSON = require("../info/Categories.json");
+    const JSON = require("../Info/Categories.json");
     const allCategories = JSON.map((category) => category.name);
-    // console.log(typesPokemon)
     allCategories.forEach((e) => {
       Categories.findOrCreate({
         where: { name: e },
